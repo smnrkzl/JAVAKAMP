3eb984bec699f59b9ecd83484864be171b9bb57b
smnrkzl <96079063+smnrkzl@users.noreply.github.com>
2022-09-28 18:09:03 +0300

    1.HAFTA

---
+++ WEEK1/arrayssDemo/src/arrayssDemo/main.java
@@ -0,0 +1,33 @@
+package arrayssDemo;
+
+public class main {
+
+	public static void main(String[] args) {
+		String ogrenci1 = "Engin";
+		String ogrenci2 = "Derin";
+		String ogrenci3 = "Salih";
+		String ogrenci4 = "Ahmet";
+		
+		System.out.println(ogrenci1);
+		System.out.println(ogrenci2);
+		System.out.println(ogrenci3);
+		System.out.println(ogrenci4);
+		
+		System.out.println("---------------");
+		
+		String[] ogrenciler = new String[4];
+		ogrenciler[1] = "Engin";
+		ogrenciler[2] = "Derin";
+		ogrenciler[3] = "Salih";
+		ogrenciler[4] = "Ahmet";
+		
+		for(int i=0; i<ogrenciler.length;i++) {
+			System.out.println(ogrenciler[i]);
+		}
+		
+        for(String ogrenci:ogrenciler) {
+        	System.out.println(ogrenci);
+        }
+	}
+
+}
